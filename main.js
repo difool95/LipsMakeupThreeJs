d81517c7f1823a5be7feebde76a490d8eab8a76b
difool95 <bilel.basly95@gmail.com>
2024-01-12 16:39:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -228,7 +228,7 @@ const SHAPELIPS = {
       float alphaClamped = ALPHARANGE.x + (ALPHARANGE.y - ALPHARANGE.x) * alpha;\n\
       \n\
       // mix colors:\n\
-      vec3 color = videoColorGs * lipstickColor;\n\
+      vec3 color = videoColorGs * lipstickColor * 1.5;\n\
       gl_FragColor = vec4(color*alphaClamped, alphaClamped);\n\
       \n\
       // DEBUG ZONE:\n\
