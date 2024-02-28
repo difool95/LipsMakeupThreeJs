10e21f3b1709a82760ed0874d751fc5ea01a7a8e
difool95 <bilel.basly95@gmail.com>
2024-02-28 09:10:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -869,7 +869,7 @@ const SHAPEFACE = {
 
 function start() {
   WebARRocksFaceShape2DHelper.init({
-    NNCPath: './neuralNets/NN_LIPS_7.json',
+    NNCPath: './neuralNets/NN_LIPS_8.json',
     canvasVideo: _canvasVideo,
     canvasAR: _canvasAR,
     shapes: [SHAPELIPS]
